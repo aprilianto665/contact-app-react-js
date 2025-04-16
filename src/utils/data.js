3d4ff037443549d1da35f932c37fd251ee0fac86
Amb4tron <aprilianto665@gmail.com>
2025-04-16 20:12:48 +0700

    creating contactapp with reactjs

---
+++ src/utils/data.js
@@ -0,0 +1,18 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "Mas Amba",
+      tag: "ambaofficial",
+      imageUrl: "/images/amba.png",
+    },
+    {
+      id: 2,
+      name: "Mas Rusdi",
+      tag: "rusdiofficial",
+      imageUrl: "/images/rusdi.png",
+    },
+  ];
+};
+
+export { getData };
